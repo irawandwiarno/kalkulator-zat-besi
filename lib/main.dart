8f8b13e3b2e2e50d1d6391778d1061ea7c8d0bed
awan <irawandwiarnop2@gmail.com>
2024-07-24 08:26:27 +0700

    Final app

--- lib/main.dart
+++ lib/main.dart
@@ -10,41 +10,21 @@ import 'package:kalkulator_zat_besi/shared/package.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseHelper.instance.database;
-  // final now = DateTime.now();
-  // print('wwww : ${now.add(Duration(days: 3))}');
-  runApp(MyApp(isExpied: await isAppExpired()));
+  runApp(MyApp());
 }
 
-Future<bool> isAppExpired() async {
-  final isExpiredPref = await Preferences().getIsExpired();
-  if (isExpiredPref != null && isExpiredPref) return true;
-
-  final now = DateTime.now();
-  final stringExpired = "2024-07-23 23:59:36.557943";
-
-  final DateFormat formatter = DateFormat('yyyy-MM-dd HH:mm:ss.SSSSSS');
-  final DateTime parsedDate = formatter.parse(stringExpired);
-  var result = now.isAfter(parsedDate);
-
-  if(result){
-    Preferences().setIsExpired(result);
-  }
-
-  return result;
-}
 
 class MyApp extends StatelessWidget {
-  MyApp({required this.isExpied, super.key});
-  final bool isExpied;
+  MyApp({ super.key});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Kalkulator',
       theme: ThemeData(fontFamily: "NunitoSans"),
-      home: isExpied ? LockScreen() : HomeView(),
+      home: HomeView(),
       getPages: AppPages.pages,
-      initialBinding:isExpied ? null : HomeBinding(),
+      initialBinding:HomeBinding(),
     );
   }
 }
